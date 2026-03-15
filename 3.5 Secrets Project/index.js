1c3fcaf0ce51b3991ccd86d28b52865e3d385223
harini bhupalam <bhupalamharini89@gmail.com>
2026-03-15 20:26:55 +0530

    first commit

---
+++ 3.5 Secrets Project/index.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+var userIsAuthorised = false; // ← MUST be here, outside everything
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === process.env.PASSWORD) {
+    userIsAuthorised = true;
+  }
+  next();
+}
+app.use(passwordCheck);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
